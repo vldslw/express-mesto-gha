5dcf53cc1184954c6dd848c7b0567e3568d25df5
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-11-24 18:33:28 -0500

    fix: исправлено добавление карточек и проставление лайков

--- controllers/card.js
+++ controllers/card.js
@@ -23,22 +23,27 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
+  .orFail(new Error('IdNotFound'))
+  .then(card => res.status(200).send({ data: card }))
   .catch((err) => {
-    if (err.name === 'CastError') {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Переданы некорректные данные при удалении карточки' });
+    } else if (err.message === 'IdNotFound') {
       res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
     } else {
       res.status(500).send({ message: 'Ошибка по умолчанию' });
     }
   });
-
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+  .orFail(new Error('IdNotFound'))
+  .then(card => res.status(200).send({ data: card }))
   .catch((err) => {
     if (err.name === 'ValidationError') {
       res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' });
-    } else if (err.name === 'CastError') {
+    } else if (err.message === 'IdNotFound') {
       res.status(404).send({ message: 'Передан несуществующий _id карточки' });
     } else {
       res.status(500).send({ message: 'Ошибка по умолчанию' });
@@ -48,10 +53,12 @@ module.exports.likeCard = (req, res) => {
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+  .orFail(new Error('IdNotFound'))
+  .then(card => res.status(200).send({ data: card }))
   .catch((err) => {
     if (err.name === 'ValidationError') {
       res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка' });
-    } else if (err.name === 'CastError') {
+    } else if (err.message === 'IdNotFound') {
       res.status(404).send({ message: 'Передан несуществующий _id карточки' });
     } else {
       res.status(500).send({ message: 'Ошибка по умолчанию' });
