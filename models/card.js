174787e2e8c2494d3b430c834af1aa0ded2873b2
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-11-24 19:39:55 -0500

    fix: исправлены ошибки в eslint

--- models/card.js
+++ models/card.js
@@ -5,25 +5,25 @@ const cardSchema = mongoose.Schema({
     type: String,
     required: true,
     minlength: 2,
-    maxlength: 30
+    maxlength: 30,
   },
   link: {
     type: String,
-    required: true
+    required: true,
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    required: true
+    required: true,
   },
   likes: {
     type: Array,
-    default: []
+    default: [],
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('card', cardSchema);
