60adfb18dbaa8115dc907ec5f897a3051b7d31ee
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-12-03 15:57:53 -0500

    feat: добавлен email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -16,6 +17,22 @@ const userSchema = mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Введите корректный адрес email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 });
 
