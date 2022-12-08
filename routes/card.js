71f1ab8b6722c13207d6678b49f6673686714bc6
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-12-07 21:37:31 -0500

    feat: добавлена валидация через celebrate для карточек и пользователей

--- routes/card.js
+++ routes/card.js
@@ -1,13 +1,31 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/card');
 const auth = require('../middlewares/auth');
 
 router.get('/cards', auth, getCards);
-router.post('/cards', auth, createCard);
-router.delete('/cards/:cardId', auth, deleteCard);
-router.put('/cards/:cardId/likes', auth, likeCard);
-router.delete('/cards/:cardId/likes', auth, dislikeCard);
+router.post('/cards', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/^(?:(?:https?|HTTPS?|ftp|FTP):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)*)(?::\d{2,})?(?:[\/?#]\S*)?$/),
+  }).unknown(true),
+}), createCard);
+router.delete('/cards/:cardId', auth, celebrate({
+  body: Joi.object().keys({
+    _id: Joi.string().hex().length(24),
+  }).unknown(true),
+}), deleteCard);
+router.put('/cards/:cardId/likes', auth, celebrate({
+  body: Joi.object().keys({
+    _id: Joi.string().hex().length(24),
+  }).unknown(true),
+}), likeCard);
+router.delete('/cards/:cardId/likes', auth, celebrate({
+  body: Joi.object().keys({
+    _id: Joi.string().hex().length(24),
+  }).unknown(true),
+}), dislikeCard);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,27 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUser, getCurrentUser, updateUser, updateAvatar,
 } = require('../controllers/user');
 const auth = require('../middlewares/auth');
 
 router.get('/users', auth, getUsers);
-router.get('/users/:userId', auth, getUser);
+router.get('/users/:userId', auth, celebrate({
+  body: Joi.object().keys({
+    _id: Joi.string().hex().length(24),
+  }).unknown(true),
+}), getUser);
 router.get('/users/me', auth, getCurrentUser);
-router.patch('/users/me', auth, updateUser);
-router.patch('/users/me/avatar', auth, updateAvatar);
+router.patch('/users/me', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }).unknown(true),
+}), updateUser);
+router.patch('/users/me/avatar', auth, celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/^(?:(?:https?|HTTPS?|ftp|FTP):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)*)(?::\d{2,})?(?:[\/?#]\S*)?$/),
+  }).unknown(true),
+}), updateAvatar);
 
 module.exports = router;
