a1a8c1f9b2c45c808a5444af855c347ed36491ae
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-12-15 11:34:44 -0500

    fix: auth errors fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/auth-err');
 
 module.exports = (req, res, next) => {
   const { cookie } = req.headers;
 
   if (!cookie || !cookie.startsWith('jwt=')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new AuthError('Необходима авторизация'));
   }
 
   const token = cookie.replace('jwt=', '');
@@ -15,9 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'de252719f27a1b244d7eac7f05feba84e6dd6122f53e103f1f65c1effce0607f');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new AuthError('Необходима авторизация'));
   }
 
   req.user = payload;
